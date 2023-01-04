bd1b0c08020f43f813ee58c5d0c61212ecf3a387
Maximilian Kehl <mkehl.dev@gmail.com>
2023-01-04 18:44:31 +0100

    Add i18n service formatting

--- src/app/services/i18n.ts
+++ src/app/services/i18n.ts
@@ -6,16 +6,20 @@ type i18nConfig = {
 class LocaleService {
   private readonly defaultLocale: string;
   private readonly availableLocales: string[];
+
   constructor(config: i18nConfig) {
     this.defaultLocale = config.defaultLocale;
     this.availableLocales = config.availableLocales;
   }
+
   getAvailableLocales() {
     return this.availableLocales;
   }
+
   getDefaultLocale() {
     return this.defaultLocale;
   }
+
   async getMessages(lang: string) {
     if (this.availableLocales.includes(lang)) {
       let messages = null;
@@ -27,9 +31,11 @@ class LocaleService {
       return messages;
     }
   }
+
   loadMessages(lang: string) {
     return import(`../../../lang/${lang}.json`);
   }
+
   isLocaleAvailable(lang: string) {
     if (this.availableLocales.includes(lang)) {
       return true;
@@ -37,6 +43,7 @@ class LocaleService {
     return false;
   }
 }
+
 export default new LocaleService({
   defaultLocale: 'en-US',
   availableLocales: ['en-US', 'de-DE', 'tr-TR']
